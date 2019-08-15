e80edcad9b5ffdddcb90a373620b8c0f349e5281
Monish <monish.deb@jmaconsulting.biz>
2019-08-15 06:29:30 -0400

    Fixed lead child ID

--- parentchildregistration.php
+++ parentchildregistration.php
@@ -443,6 +443,9 @@ function parentchildregistration_civicrm_postProcess($formName, &$form) {
     // Check if contact has child with lead family member. If he doesn't then add first child as lead member.
     $isLeadFamilyPresent = CRM_Core_DAO::singleValueQuery("SELECT n.lead_family_member__28 FROM civicrm_value_newsletter_cu_3 n INNER JOIN civicrm_relationship r ON n.entity_id = r.contact_id_a WHERE r.relationship_type_id = 1 AND r.contact_id_b = %1 AND n.lead_family_member__28 = 1 LIMIT 1", [1 => [$parent, 'Integer']]);
     if (empty($isLeadFamilyPresent)) {
+      if (is_array($leadChildId)) {
+        $leadChildId = $leadChildId[0];
+      }
       civicrm_api3('Contact', 'create', ['id' => $leadChildId, LEAD_MEMBER => 1]);
     }
 
