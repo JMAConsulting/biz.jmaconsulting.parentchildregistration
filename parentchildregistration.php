2355d332d3b2c33130dc599034cc696fce916b28
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-08-04 03:30:25 -0400

    Ensure that we return the service region not region

--- parentchildregistration.php
+++ parentchildregistration.php
@@ -688,10 +688,10 @@ function parentchildregistration_civicrm_postProcess($formName, &$form) {
 
 function getChapRegCodes($postalCode) {
   $chapterCode = strtoupper(substr($postalCode, 0, 3));
-  $sql = "SELECT pcode, region, chapter FROM chapters_lookup WHERE pcode = '{$chapterCode}'";
+  $sql = "SELECT pcode, service_region, chapter FROM chapters_lookup WHERE pcode = '{$chapterCode}'";
   $dao = CRM_Core_DAO::executeQuery($sql);
   while ($dao->fetch()) {
-    $region = $dao->region;
+    $region = $dao->service_region;
     $chapter = $dao->chapter;
   }
   return [$chapter, $region];
